b72c97c5de72dc50227e5e3c2e522fec792a8278
Ioseph Magno <83366073+iosephmagno@users.noreply.github.com>
2022-08-07 17:57:08 +0200

    Add files via upload

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_native_text_input/flutter_native_text_input.dart';
+import 'package:flutter_native_text_input_example/chat_screen.dart';
 import 'package:flutter_native_text_input_example/demo_item.dart';
 import 'package:flutter_native_text_input_example/more_use_case_listing_page.dart';
 
@@ -21,6 +22,7 @@ class _MyAppState extends State<MyApp> {
 }
 
 class HomePage extends StatelessWidget {
+  final _controller = TextEditingController();
   final FocusNode _focusNode = FocusNode();
 
   _onChangeText(value) => debugPrint("_onChangeText: $value");
@@ -33,12 +35,84 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Demo Page'),
       ),
-      body: Column(
-        children: [
-          Expanded(child: Center(child: Text("Body"))),
+      body: ListView(
+        children: <Widget>[
+          DemoItem(
+            title: 'Flutter TextField Example Usage',
+            child: TextField(
+              onChanged: _onChangeText,
+              onSubmitted: _onSubmittedText,
+              autocorrect: true,
+              decoration: const InputDecoration(
+                hintText: 'placeholder',
+                border: InputBorder.none,
+              ),
+            ),
+          ),
+          DemoItem(
+            title: 'Flutter CupertinoTextField Example Usage',
+            child: CupertinoTextField(
+              autocorrect: true,
+              cursorColor: Colors.black87,
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Colors.black87,
+                  width: 2,
+                ),
+              ),
+              style: const TextStyle(
+                fontSize: 16,
+                color: Colors.black54,
+              ),
+              textCapitalization: TextCapitalization.sentences,
+              placeholderStyle: const TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+                color: Colors.black12,
+              ),
+              placeholder: 'placeholder',
+              onChanged: _onChangeText,
+              onSubmitted: _onSubmittedText,
+            ),
+          ),
+          DemoItem(
+            title: 'NativeTextInput Example Usage',
+            child: Container(
+              height: 30,
+              child: NativeTextInput(
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.black87,
+                    width: 2,
+                  ),
+                ),
+                style: const TextStyle(
+                  fontSize: 16,
+                  color: Colors.black54,
+                ),
+                minHeightPadding: 4,
+                textCapitalization: TextCapitalization.sentences,
+                placeholder: "placeholder",
+                placeholderColor: Colors.black12,
+                iosOptions: IosOptions(
+                  autocorrect: true,
+                  cursorColor: Colors.black87,
+                  keyboardAppearance: Brightness.dark,
+                  placeholderStyle: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                keyboardType: KeyboardType.defaultType,
+                onChanged: _onChangeText,
+                onSubmitted: _onSubmittedText,
+                focusNode: _focusNode,
+              ),
+            ),
+          ),
           DemoItem(
             title: "Multiline Text Input",
             child: NativeTextInput(
+              controller: _controller,
               minHeightPadding: 14,
               minLines: 1,
               maxLines: 6,
@@ -56,6 +130,22 @@ class HomePage extends StatelessWidget {
                 print("onTap");
               },
             ),
+          ),
+          Center(
+            child: ElevatedButton(
+                child: const Text('View More Use Cases'),
+                onPressed: () {
+                  Navigator.of(context).push(MaterialPageRoute(
+                      builder: (_) => const MoreUseCaseListingPage()));
+                }),
+          ),
+          Center(
+            child: ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).push(
+                      MaterialPageRoute(builder: (_) => const ChatScreen()));
+                },
+                child: const Text('Chat screen')),
           ),
         ],
       ),
--- example/lib/more_use_case_listing_page.dart
+++ example/lib/more_use_case_listing_page.dart
@@ -16,9 +16,7 @@ class _MoreUseCaseListingPageState extends State<MoreUseCaseListingPage> {
   String _currentTextInput = '';
 
   _onChangeText(value) => debugPrint("_onChangeText: $value");
-
   _onSubmittedText(value) => debugPrint("_onSubmittedText: $value");
-
   _onTap(BuildContext context) {
     const snackBar = SnackBar(content: Text('Tapped!'));
     ScaffoldMessenger.of(context).showSnackBar(snackBar);
@@ -27,30 +25,153 @@ class _MoreUseCaseListingPageState extends State<MoreUseCaseListingPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("More Use Cases"),
-      ),
-      body: DemoItem(
-          title: "Multiline Text Input",
-          child: NativeTextInput(
-            minHeightPadding: 14,
-            minLines: 1,
-            maxLines: 6,
-            keyboardType: KeyboardType.defaultType,
-            returnKeyType: ReturnKeyType.defaultAction,
-            iosOptions: IosOptions(
-              keyboardAppearance: Brightness.light,
-            ),
-            placeholder: "S().message",
-            textCapitalization:
-            TextCapitalization.sentences,
-            onChanged: (val) {
-              print("onChanged => $val");
-            },
-            onTap: () {
-              print("onTap");
-            },
-          )),
-    );
+        appBar: AppBar(
+          title: const Text("More Use Cases"),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              DemoItem(
+                  title: "No Placeholder",
+                  child: NativeTextInput(
+                    onChanged: _onChangeText,
+                    onSubmitted: _onSubmittedText,
+                  )),
+              DemoItem(
+                  title: "Custom font",
+                  child: NativeTextInput(
+                    style: const TextStyle(
+                      fontFamily: 'Noteworthy',
+                    ),
+                    placeholder: 'Noteworthy',
+                    iosOptions: IosOptions(
+                      placeholderStyle: const TextStyle(
+                        fontFamily: 'Noteworthy',
+                      ),
+                    ),
+                  )),
+              DemoItem(
+                  title: "Numeric Keyboard",
+                  child: Column(
+                    children: <Widget>[
+                      NativeTextInput(
+                        keyboardType: KeyboardType.decimalPad,
+                        onChanged: _onChangeText,
+                        onSubmitted: _onSubmittedText,
+                      ),
+                    ],
+                  )),
+              DemoItem(
+                  title: "Pre-Filling Text",
+                  child: NativeTextInput(
+                    controller: TextEditingController(text: "Text"),
+                    onChanged: _onChangeText,
+                    onSubmitted: _onSubmittedText,
+                  )),
+              DemoItem(
+                title: "Aligning Text to End",
+                child: NativeTextInput(
+                  textAlign: TextAlign.end,
+                  controller: TextEditingController(text: "Text"),
+                  onChanged: _onChangeText,
+                  onSubmitted: _onSubmittedText,
+                ),
+              ),
+              DemoItem(
+                  title: "Multiline Text Input",
+                  child: NativeTextInput(
+                    minLines: 3,
+                    maxLines: 5,
+                    returnKeyType: ReturnKeyType.defaultAction,
+                    onChanged: _onChangeText,
+                    onSubmitted: _onSubmittedText,
+                  )),
+              DemoItem(
+                title: "Displaying Input Value",
+                child: Column(children: [
+                  NativeTextInput(
+                    placeholder: "Type something here",
+                    onChanged: (value) {
+                      _onChangeText(value);
+                      setState(() {
+                        _currentTextInput = value;
+                      });
+                    },
+                    onSubmitted: _onSubmittedText,
+                  ),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text('Current Input: '),
+                      Expanded(child: Text(_currentTextInput)),
+                    ],
+                  )
+                ]),
+              ),
+              DemoItem(
+                  title: "Focusing or Unfocusing Text Input",
+                  child: Column(
+                    children: [
+                      FlatButton(
+                          color: Colors.blue,
+                          colorBrightness: Brightness.dark,
+                          child: const Text("Tap Me!"),
+                          onPressed: () {
+                            if (_focusNode.hasFocus) {
+                              FocusScope.of(context).unfocus();
+                            } else {
+                              FocusScope.of(context).requestFocus(_focusNode);
+                            }
+                          }),
+                      NativeTextInput(
+                        focusNode: _focusNode,
+                        onChanged: _onChangeText,
+                        onSubmitted: _onSubmittedText,
+                      )
+                    ],
+                  )),
+              DemoItem(
+                  title: "Filling Text Programmatically",
+                  child: Column(
+                    children: [
+                      FlatButton(
+                        color: Colors.blue,
+                        colorBrightness: Brightness.dark,
+                        child: const Text("Tap Me!"),
+                        onPressed: () => _changeTextController.text =
+                            DateTime.now().toString(),
+                      ),
+                      NativeTextInput(
+                        controller: _changeTextController,
+                        onChanged: _onChangeText,
+                        onSubmitted: _onSubmittedText,
+                      )
+                    ],
+                  )),
+              DemoItem(
+                  title: "Recognizing Tap",
+                  child: NativeTextInput(
+                    onTap: () => _onTap(context),
+                  ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(100),
+                child: Center(
+                    child: Column(children: const [
+                  Text(
+                    'All done!',
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    textAlign: TextAlign.center,
+                  ),
+                  Text(
+                    'Enjoy using in your way!',
+                    style: TextStyle(fontSize: 17),
+                    textAlign: TextAlign.center,
+                  )
+                ])),
+              ),
+            ],
+          ),
+        ));
   }
 }
